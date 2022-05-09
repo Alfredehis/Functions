ade1b8797aaeae19069e724172b7d2d6223a646c
Alfredehis <102754266+Alfredehis@users.noreply.github.com>
2022-05-09 14:13:08 +0100

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,95 @@
+// let score = 19
+// if (score <20) {console.log ("You Failed")}
+// else {console.log ("You Passed")}
+
+
+// const passedMark = 20
+// if (passedMark <20) {console.log ("Good")}
+// else {console.log ("Bad")}
+
+
+// // FUNCTIONS ()
+// function cleanShoe ()
+// {console.log ("I am cleaning shoe")}
+// cleanShoe ()
+
+// function addScores ()
+// {let addScores = 9
+// console.log (addScores)}
+// addScores ()
+
+// function comeIn (a)
+// {console.log (`You are welcome ${a}`)}
+// comeIn ("Tosin")
+
+// function welcome (a, b)
+// {console.log (`${a} is related to ${b}`)}
+// welcome ("David", "Freddy")
+
+// // ARROW FUNCTION  () => {}
+
+// const addSum = (age) => 
+// {age > 18 ? console.log ("Adult") : console.log ("Logout")}
+// addSum (17)
+// addSum (21)
+// addSum (30)
+// addSum (12)
+
+
+
+// const age = 45
+// if (age <10) console.log ("You are a child")
+// else if (age <=17) console.log ("You are a tenager")
+// else if (age <=35) console.log ("You are a youth")
+// else if (age <=50) console.log ("You are matured")
+// else console.log ("You are old")
+
+
+
+// ASSIGNMENT 
+//  Print out names of students in a department 
+
+const nameOfStudents = ["Sandra", "Johnson", "Francis", "Emmanuel", "Francisca", "Joseph", "Helen", "Jude", "Tega", "Harrison"]
+console.log (nameOfStudents)
+
+// Calculate Area of a circle using pi r**2
+// Where
+// pi = 3.142, r = 15, r = 5, r = 7
+
+const pi = 3.142
+const r = 15
+let area = (pi * r**2)
+console.log (area)
+
+const pi1 = 3.142
+const r1 = 5
+let area1 = (pi1 * r1**2)
+console.log (area1)
+
+const pi2 = 3.142
+const r2 = 7
+let area2 = (pi2 * r2**2)
+console.log (area2)
+
+// Number of goals of different clubs with winners using Arrow Functions
+
+// WHERE:
+// Chelsea = 45
+// Arsenal = 30
+// Liverpool = 19
+
+const footBall = (score) =>
+{if (score <20) console.log ("Third")
+else if (score <=35) console.log ("Second")
+else if (score <=50) console.log ("Winner")
+else console.log ()
+}
+
+footBall (19) 
+footBall (30) 
+footBall (45) 
+
+
+
+
+
